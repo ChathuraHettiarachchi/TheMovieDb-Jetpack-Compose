baddf1cf82ffc762f51636b965a30304ef07ccd3
Chathura <chathura93@yahoo.com>
2022-04-11 22:35:11 +0530

    - common page title added

---
+++ app/src/main/java/com/backbase/moviebox/presentation/common_components/ScreenTitle.kt
@@ -0,0 +1,45 @@
+package com.backbase.moviebox.presentation.common_components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.backbase.moviebox.theme.text
+
+@Composable
+fun ScreenTitle(title: String, isCentered: Boolean = false) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+
+    ) {
+        Spacer(Modifier.height(20.dp))
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            color = text,
+            text = title,
+            textAlign = if (isCentered) TextAlign.Center else TextAlign.Left,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(Modifier.height(20.dp))
+    }
+}
+
+@Composable
+@Preview
+fun ScreenTitlePreview() {
+    ScreenTitle("Playing Now")
+}
+
+@Composable
+@Preview
+fun ScreenTitlePreviewCenter() {
+    ScreenTitle("Playing Now", true)
+}
