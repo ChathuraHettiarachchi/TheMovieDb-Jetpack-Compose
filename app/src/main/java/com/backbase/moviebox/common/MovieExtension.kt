9779ca8cef2ad37f315860b02cfcf77a7af85eed
Chathura <chathura93@yahoo.com>
2022-04-12 17:15:55 +0530

    - pager with offset and animation

--- app/src/main/java/com/backbase/moviebox/common/MovieExtension.kt
+++ app/src/main/java/com/backbase/moviebox/common/MovieExtension.kt
@@ -1,9 +1,32 @@
 package com.backbase.moviebox.common
 
 import android.content.res.Resources
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 val Int.toPx get() = (this * Resources.getSystem().displayMetrics.density).toInt()
 
 val Int.toDp get() = (this / Resources.getSystem().displayMetrics.density).toInt()
 
 fun String.addEmptyLines(lines: Int) = this + "\n".repeat(lines)
+
+/**
+ * Linearly interpolate between [start] and [stop] with [fraction] fraction between them.
+ */
+fun lerp(start: Float, stop: Float, fraction: Float): Float {
+    return (1 - fraction) * start + fraction * stop
+}
+
+/**
+ * Linearly interpolate between [start] and [stop] with [fraction] fraction between them.
+ */
+fun lerp(start: Int, stop: Int, fraction: Float): Int {
+    return start + ((stop - start) * fraction.toDouble()).roundToInt()
+}
+
+/**
+ * Linearly interpolate between [start] and [stop] with [fraction] fraction between them.
+ */
+fun lerp(start: Long, stop: Long, fraction: Float): Long {
+    return start + ((stop - start) * fraction.toDouble()).roundToLong()
+}
--- app/src/main/java/com/backbase/moviebox/presentation/most_popular/MostPopularScreen.kt
+++ app/src/main/java/com/backbase/moviebox/presentation/most_popular/MostPopularScreen.kt
@@ -1,40 +1,50 @@
 package com.backbase.moviebox.presentation.most_popular
 
-import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.backbase.moviebox.common.toDp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
+import com.backbase.moviebox.R
+import com.backbase.moviebox.common.Constants
+import com.backbase.moviebox.common.lerp
 import com.backbase.moviebox.domain.model.Movie
-import com.backbase.moviebox.navigation.MovieScreenList
 import com.backbase.moviebox.presentation.common_components.ErrorView
 import com.backbase.moviebox.presentation.common_components.LoadingView
+import com.backbase.moviebox.presentation.common_components.MoviePosterView
 import com.backbase.moviebox.presentation.common_components.ScreenTitleView
 import com.backbase.moviebox.presentation.most_popular.components.PopularMovieInfoView
-import com.backbase.moviebox.presentation.movie_details.MovieDetailViewModel
-import com.backbase.moviebox.presentation.playing_now.components.MovieItemView
 import com.backbase.moviebox.theme.primary
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.calculateCurrentOffsetForPage
+import com.google.accompanist.pager.rememberPagerState
+import kotlin.math.absoluteValue
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun MostPopularScreen(
     navController: NavController?,
     viewModel: MostPopularViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
+    val pagerState = rememberPagerState()
+    val pages = mutableListOf<Movie>()
 
     Box(
         modifier = Modifier
@@ -43,21 +53,50 @@ fun MostPopularScreen(
     ) {
         Column(modifier = Modifier.fillMaxSize()) {
             ScreenTitleView(title = "Most popular", paddingStart = 16)
-            Box(modifier = Modifier
-                .background(Color.Red)
-                .fillMaxWidth()
-                .weight(2f, true)){
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(2f, true)
+            ) {
+                if (!state.isLoading) {
+                    pages.addAll(state.data.results)
+                    HorizontalPager(count = pages.size, state = pagerState, contentPadding = PaddingValues(horizontal = 64.dp)) { page ->
+                        Column(
+                            modifier = Modifier.fillMaxSize().graphicsLayer {
+                                val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+                                lerp(
+                                    start = 0.85f,
+                                    stop = 1f,
+                                    fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                                ).also { scale ->
+                                    scaleX = scale
+                                    scaleY = scale
+                                }
+
+                                // We animate the alpha, between 50% and 100%
+                                alpha = lerp(
+                                    start = 0.5f,
+                                    stop = 1f,
+                                    fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                                )
+                            },
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            MoviePosterView(posterPath = pages[page].poster_path, isDetails = true)
+                        }
+                    }
+                }
             }
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f, true)) {
-                PopularMovieInfoView(movie =  Movie(
-                    id = 675353,
-                    original_title = "Sonic the Hedgehog 2",
-                    overview = "After settling in Green Hills, Sonic is eager to prove he has what it takes to be a true hero. His test comes when Dr. Robotnik returns, this time with a new partner, Knuckles, in search for an emerald that has the power to destroy civilizations. Sonic teams up with his own sidekick, Tails, and together they embark on a globe-trotting journey to find the emerald before it falls into the wrong hands.",
-                    vote_average = 5.6,
-                    poster_path = "/6DrHO1jr3qVrViUO6s6kFiAGM7.jpg"
-                ), navController = navController)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f, true)
+            ) {
+                PopularMovieInfoView(
+                    movie = if (pages.size > 0) pages[pagerState.currentPage] else Movie(),
+                    navController = navController
+                )
             }
         }
     }
