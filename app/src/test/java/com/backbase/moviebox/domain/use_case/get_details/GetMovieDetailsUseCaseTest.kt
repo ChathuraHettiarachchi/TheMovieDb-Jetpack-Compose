c9f54918b5cf1900e22e3f0bfa7566756046370c
Chathura <chathura93@yahoo.com>
2022-04-13 11:52:22 +0530

    - movieDetails viewModel + usecase

---
+++ app/src/test/java/com/backbase/moviebox/domain/use_case/get_details/GetMovieDetailsUseCaseTest.kt
@@ -0,0 +1,5 @@
+package com.backbase.moviebox.domain.use_case.get_details
+
+import org.junit.Assert.*
+
+class GetMovieDetailsUseCaseTest
---
+++ app/src/test/java/com/backbase/moviebox/presentation/movie_details/MovieDetailViewModelTest.kt
@@ -0,0 +1,5 @@
+package com.backbase.moviebox.presentation.movie_details
+
+import org.junit.Assert.*
+
+class MovieDetailViewModelTest
